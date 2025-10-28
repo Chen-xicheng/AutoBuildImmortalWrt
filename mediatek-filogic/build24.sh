8ba3d6a0d5d83671b5654470b3677941cb4ae9de
CHENG <chenxicheng@live.com>
2025-10-28 15:11:52 +0800

    Update build24.sh

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -74,7 +74,6 @@ PACKAGES="$PACKAGES luci-app-ttyd"
 PACKAGES="$PACKAGES luci-app-diskman"
 PACKAGES="$PACKAGES luci-app-partexp"
 PACKAGES="$PACKAGES luci-app-airplay2"
-PACKAGES="$PACKAGES luci-app-iptvhelper"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
